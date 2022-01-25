b6f19035a53a61b2560ea9ae6a9b1ac4d0e698f3
Auto Mation <automation@commercetools.com>
2022-01-25 10:54:01 +0000

    TASK: Updating SDK

--- commercetools/commercetools-sdk-java-ml/src/main/java-generated/com/commercetools/ml/models/common/Reference.java
+++ commercetools/commercetools-sdk-java-ml/src/main/java-generated/com/commercetools/ml/models/common/Reference.java
@@ -14,9 +14,6 @@ import io.vrap.rmf.base.client.utils.Generated;
 
 @JsonSubTypes({
         @JsonSubTypes.Type(value = com.commercetools.ml.models.common.CategoryReferenceImpl.class, name = CategoryReference.CATEGORY),
-        @JsonSubTypes.Type(value = com.commercetools.ml.models.common.CategoryReferenceImpl.class, name = CategoryReference.CATEGORY),
-        @JsonSubTypes.Type(value = com.commercetools.ml.models.common.ProductReferenceImpl.class, name = ProductReference.PRODUCT),
-        @JsonSubTypes.Type(value = com.commercetools.ml.models.common.ProductReferenceImpl.class, name = ProductReference.PRODUCT),
         @JsonSubTypes.Type(value = com.commercetools.ml.models.common.ProductReferenceImpl.class, name = ProductReference.PRODUCT),
         @JsonSubTypes.Type(value = com.commercetools.ml.models.common.ProductTypeReferenceImpl.class, name = ProductTypeReference.PRODUCT_TYPE) })
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "typeId", defaultImpl = ReferenceImpl.class, visible = true)
