6f79edd76516a270d87c9b1a4dbf757d268d6849
Auto Mation <automation@commercetools.com>
2022-06-23 07:34:43 +0000

    spotless: Fix code style

--- commercetools/commercetools-sdk-compat-v1/src/jmh/java/com/commercetools/benchmark/ClientBenchmark.java
+++ commercetools/commercetools-sdk-compat-v1/src/jmh/java/com/commercetools/benchmark/ClientBenchmark.java
@@ -1,4 +1,7 @@
+
 package com.commercetools.benchmark;
+
+import java.time.Duration;
 
 import com.commercetools.api.client.ProjectApiRoot;
 import com.commercetools.api.defaultconfig.ApiRootBuilder;
@@ -6,14 +9,14 @@ import com.commercetools.api.defaultconfig.ServiceRegion;
 import com.commercetools.http.apachehttp.CtApacheHttpClient;
 import com.commercetools.http.asynchttp.CtAsyncHttpClient;
 import com.commercetools.http.okhttp4.CtOkHttp4Client;
+
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientFactory;
 import io.sphere.sdk.projects.queries.ProjectGet;
 import io.vrap.rmf.base.client.oauth2.ClientCredentials;
-import org.openjdk.jmh.annotations.*;
 
-import java.time.Duration;
+import org.openjdk.jmh.annotations.*;
 
 public class ClientBenchmark {
     @State(Scope.Benchmark)
@@ -30,27 +33,26 @@ public class ClientBenchmark {
         public void init() {
             ahcApiRoot = ApiRootBuilder.of(new CtAsyncHttpClient())
                     .defaultClient(
-                            ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
-                            ServiceRegion.GCP_EUROPE_WEST1)
+                        ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
+                        ServiceRegion.GCP_EUROPE_WEST1)
                     .build(getProjectKey());
 
             apacheApiRoot = ApiRootBuilder.of(new CtApacheHttpClient())
                     .defaultClient(
-                            ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
-                            ServiceRegion.GCP_EUROPE_WEST1)
+                        ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
+                        ServiceRegion.GCP_EUROPE_WEST1)
                     .build(getProjectKey());
 
             okhttpApiRoot = ApiRootBuilder.of(new CtOkHttp4Client())
                     .defaultClient(
-                            ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
-                            ServiceRegion.GCP_EUROPE_WEST1)
+                        ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
+                        ServiceRegion.GCP_EUROPE_WEST1)
                     .build(getProjectKey());
 
             final SphereClientFactory factory = SphereClientFactory.of();
-            final SphereClient client = factory.createClient(
-                    getProjectKey(), //replace with your project key
-                    getClientId(), //replace with your client id
-                    getClientSecret()); //replace with your client secret
+            final SphereClient client = factory.createClient(getProjectKey(), //replace with your project key
+                getClientId(), //replace with your client id
+                getClientSecret()); //replace with your client secret
 
             sphereClient = BlockingSphereClient.of(client, Duration.ofSeconds(10));
         }
